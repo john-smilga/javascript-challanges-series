4484b765783552e7de4ea3f70707c8cd1b8d45d5
john-smilga <johnsmilga@gmail.com>
2021-05-24 14:50:53 -0700

    02-multiply-recorded

--- challanges/02-multiply-args.js
+++ challanges/02-multiply-args.js
@@ -1,19 +1,23 @@
 // Quokka.js extension
 //"CMD + K + Q" - Existing File, "CMD + K + J" - New File
+// multiply arguments, unlimited arguments, if 0
 
 // function multiply() {
 //   const args = [...arguments]
-//   return args.reduce((total, item) => {
+//   const result = args.reduce((total, item) => {
 //     return item ? item * total : item + total
 //   }, 1)
-//   return 120
+//   return result
 // }
 
-const multiply = (...args) => {
-  return args.reduce((total, item) => {
+function multiply(...args) {
+  const result = args.reduce((total, item) => {
     return item ? item * total : item + total
   }, 1)
+  return result
 }
+
+multiply(2, 3, 4, 5, 6, 7)
 
 console.log(multiply(4, 5))
 // 4 * 5 = 20
